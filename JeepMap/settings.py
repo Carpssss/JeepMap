b16812cd34f3e38048cd3a1f7754b7b72ceb16cd
Chowder <jamilaarongcarapatan@iskolarngbayan.pup.edu.ph>
2025-07-27 05:37:16 +0800

    feat: incomplete ui for admin master

--- JeepMap/settings.py
+++ JeepMap/settings.py
@@ -106,15 +106,13 @@ WSGI_APPLICATION = 'JeepMap.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'jeepmap',
+        'USER': 'root',   
+        'PASSWORD': 'mysql123#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
