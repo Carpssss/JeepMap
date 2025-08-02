014d877d6e927d77937919411a6046a28e7662f6
Chowder <jamilaarongcarapatan@iskolarngbayan.pup.edu.ph>
2025-08-02 22:09:42 +0800

    Feat: Schedule Master

--- crewMaster/urls.py
+++ crewMaster/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'crew_master'
 urlpatterns = [
-    path('', views.crew_list, name='crew_master_list'),
+    path('', views.crew_list, name='list'),
     path('create/', views.crew_create, name='create'),
     path('<int:pk>/', views.crew_detail, name='detail'),
     path('<int:pk>/update/', views.crew_update, name='update'),
